dddac7f5f14c1528f7684bfe2cbec9711d2acb60
dqgthb <dqgthb@users.noreply.github.com>
2020-12-17 17:30:53 +0900

    .

--- 15654/main.py
+++ 15654/main.py
@@ -42,43 +42,9 @@ def pfast(*args, end = "\n", sep=' '):
 
 def ints(): return map(int, sys.stdin.readline().rstrip().split())
 
-def solve(mat):
-    n = len(mat)
-    for i in range(n):
-        mat[i][i] = 0
-
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                mat[i][j] = min(mat[i][k] + mat[k][j], mat[i][j])
-            
-def prmat(mat):
-    for i in mat:
-        print(i)
-
 def main(f = None):
     init(f)
-    n = int(input().strip())
-    m = int(input().strip())
-
-    mat = [[float("inf") for _ in range(n)] for _ in range(n)]
-    for _ in range(m):
-        a, b, c = (int(i) for i in input().split())
-        a = a-1
-        b = b-1
-        mat[a][b] = min(mat[a][b], c)
-
-    solve(mat)
-
-
-    for i in range(n):
-        for j in range(n):
-            a = mat[i][j]
-            if a == float("inf"):
-                a = 0
-            print(a, end=' ')
-        print()
-
+    pass
 
 if __name__ == "__main__":
     main()
