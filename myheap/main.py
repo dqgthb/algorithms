e8168587b56411ebf29990b7387194b54982cb19
dqgthb <dqgthb@users.noreply.github.com>
2020-12-19 13:17:08 +0900

    .

---
+++ myheap/main.py
@@ -0,0 +1,14 @@
+class myheap:
+    def __init__(self):
+        self.arr = []
+    
+    def push(self, n):
+        self.arr.append(n)
+
+
+def main():
+    h = myheap()
+
+
+if __name__ == "__main__":
+    main()
