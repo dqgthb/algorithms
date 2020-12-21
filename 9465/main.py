8e06f99e28ad49dbe34e9d7f11684a2d3c2e7552
dk <kimdhyungg@gmail.com>
2020-12-21 10:09:53 +0900

    .

--- 9465/main.py
+++ 9465/main.py
@@ -46,8 +46,13 @@ def solve():
     
 
 class DP():
-    def __init__(s, l):
-        dp = [[None for _ in range(l)] for _ in range(2)]
+    def __init__(s, l, mat):
+        s.dp = [[None for _ in range(l)] for _ in range(2)]
+        s.mat = mat
+    
+    def solve(s, i, j):
+        if s.dp[i][j] is not None: return s.dp[i][j]
+
 
 def main(f = None):
     global mat
@@ -56,7 +61,7 @@ def main(f = None):
     for _ in range(t):
         l = int(input().strip())
         mat = [list(map(int, input().split())) for _ in range(2)]
-        s = Solution(l)
+        s = DP(l, mat)
         ans = solve()
 
 if __name__ == "__main__":
