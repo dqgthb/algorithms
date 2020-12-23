b8cd36f7e3f0edf47fbcae18e90af503180070ec
dqgthb <dqgthb@users.noreply.github.com>
2020-12-23 17:23:54 +0900

    .

--- 1915/main.py
+++ 1915/main.py
@@ -50,7 +50,15 @@ def main(f = None):
 
     dp = [[0 for _ in range(m)] for _ in range(n)]
     for i in range(n):
-        if mat[i]
+        if mat[i][0] == 1:
+            dp[i][0] = 1
+        if mat[0][i] == 1:
+            dp[0][i] = 1
+    for i in dp:
+        print(i)
+    
+    for i in range(1, n):
+        for j in range(1, m):
 
 
 
