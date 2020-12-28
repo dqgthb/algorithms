0912c5149d55ac7950dea4e8e067ae900e7c50f2
dk <kimdhyungg@gmail.com>
2020-12-28 11:15:52 +0900

    .

--- 11437/main.py
+++ 11437/main.py
@@ -45,7 +45,10 @@ def ints(): return map(int, sys.stdin.readline().rstrip().split())
 def main(f = None):
     init(f)
     N = int(input())
+    g = [[] for _ in range(N)]
     for _ in range(N-1):
+        a, b = map(lambda x: int(x) - 1, input().split())
+        g[a].append(b)
 
 if __name__ == "__main__":
     main()
