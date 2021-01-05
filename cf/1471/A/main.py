cf6ebe35938ceb85ce2248bb514fa08c6c490667
dqgthb <dqgthb@users.noreply.github.com>
2021-01-05 23:37:53 +0900

    .

--- cf/1471/A/main.py
+++ cf/1471/A/main.py
@@ -13,29 +13,17 @@ def setStdin(f):
     input=sys.stdin.readline
 
 def init(f = None):
-    if os.path.exists("o"):
-        sys.stdout = open("o", "w")
-
-    if f is not None:
-        setStdin(f)
+    if os.path.exists("o"): sys.stdout = open("o", "w")
+    if f is not None: setStdin(f)
     else:
         if len(sys.argv) == 1:
-
-            if os.path.isfile("in/i"):
-                setStdin("in/i")
-
-            elif os.path.isfile("i"):
-                setStdin("i")
-
-        elif len(sys.argv) == 2:
-            setStdin(sys.argv[1])
-
-        else:
-            assert False, "Too many sys.argv: %d" % len(sys.argv)
+            if os.path.isfile("in/i"): setStdin("in/i")
+            elif os.path.isfile("i"): setStdin("i")
+        elif len(sys.argv) == 2: setStdin(sys.argv[1])
+        else: assert False, "Too many sys.argv: %d" % len(sys.argv)
 
 def dprint(*args):
-    if DEBUG:
-        print(*args)
+    if DEBUG: print(*args)
 
 def pfast(*args, end = "\n", sep=' '):
     sys.stdout.write(sep.join(map(str, args)) + end)
@@ -44,7 +32,6 @@ def ints(): return map(int, sys.stdin.readline().rstrip().split())
 
 def main(f = None):
     init(f)
-    pass
 
 if __name__ == "__main__":
     main()
