54654013fd7ceb75b133fbece486f6fcd6c503b2
dk <kimdhyungg@gmail.com>
2021-01-06 15:17:53 +0900

    .

--- 10844/main.py
+++ 10844/main.py
@@ -188,10 +188,14 @@ def main(f = None):
     for i in range(10):
         dp[1][i] = 1
     for i in range(2, 100):
-        j = 1
+        j = 0
+        dp[i][j] = dp[i-1][1]
         for j in range(1, 9):
             dp[i][j] = MOD.add(dp[i-1][j-1], dp[i-1][j+1])
         j = 9
+        dp[i][j] = dp[i-1][8]
+    
+    print(sum(dp[n]))
 
 if __name__ == "__main__":
     main()
