95a21c3e8ac9fd41fbe24e6efc9534fbd82e6bd6
dk <kimdhyungg@gmail.com>
2021-01-06 15:37:53 +0900

    .

--- 10844/main.py
+++ 10844/main.py
@@ -188,7 +188,7 @@ def main(f = None):
     dp = [[None for _ in range(10)] for _ in range(n+1)]
     for i in range(10):
         dp[1][i] = 1
-    for i in range(2, 100):
+    for i in range(2, n):
         j = 0
         dp[i][j] = dp[i-1][1]
         for j in range(1, 9):
