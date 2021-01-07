8685389b8007f109d17ce3616c83afbdd24670ce
dqgthb <dqgthb@users.noreply.github.com>
2021-01-08 00:24:52 +0900

    .

--- 1865/main.py
+++ 1865/main.py
@@ -176,7 +176,7 @@ def pfast(*args, end = "\n", sep=' '): sys.stdout.write(sep.join(map(str, args))
 
 def solve(N, M, W):
     g = [[] for _ in range(N)]
-    for _ in range(M):
+    for _ in range(M+W):
         s, e, t = map(int, input().split())
         g[s].append((e, t))
 
