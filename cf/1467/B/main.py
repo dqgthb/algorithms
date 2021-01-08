c68daf33ee8385fbb91be96bf54c0bbe99945dc8
dqgthb <dqgthb@users.noreply.github.com>
2021-01-09 00:53:52 +0900

    .

--- cf/1467/B/main.py
+++ cf/1467/B/main.py
@@ -214,8 +214,8 @@ def main(f = None):
             for i in range(1, n-1):
                 gi = geo[i]
                 gj = geo[i+1]
-                if gi == VALLEY nad gj == 
-                    if 
+                if gi == VALLEY and gj == HILL:
+
             return count - 1
 
         ans = solve()
