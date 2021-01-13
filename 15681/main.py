2db7db00ac3030e082bb134c7d822aefdb135a5d
dk <kimdhyungg@gmail.com>
2021-01-13 16:55:53 +0900

    .

--- 15681/main.py
+++ 15681/main.py
@@ -1,10 +1,32 @@
-# CP template Version 1.005
+# CP template Version .005
 import os
 import sys
 sys.setrecursionlimit(10**9)
 DEBUG = False
 
-input = sys.stdin.readline
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input=sys.stdin.readline
+
+def init(f = None):
+    if os.path.exists("o"): sys.stdout = open("o", "w")
+    if f is not None: setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"): setStdin("in/i")
+            elif os.path.isfile("i"): setStdin("i")
+        elif len(sys.argv) == 2: setStdin(sys.argv[1])
+        else: assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+def dprint(*args):
+    if DEBUG: print(*args)
+
+def pfast(*args, end = "\n", sep=' '): sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+init(None)
 
 N, R, Q = map(int, input().split())
 R -= 1
