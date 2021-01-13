39a362dd1857bb76c0b56b551124bc3d1ffb5412
dk <kimdhyungg@gmail.com>
2021-01-13 16:53:53 +0900

    .

--- 15681/main.py
+++ 15681/main.py
@@ -1,7 +1,6 @@
 # CP template Version 1.005
-import os
 import sys
-sys.setrecursionlimit(987654321)
+sys.setrecursionlimit(10**9)
 DEBUG = False
 
 def setStdin(f):
@@ -9,16 +8,6 @@ def setStdin(f):
     DEBUG = True
     sys.stdin = open(f)
     input=sys.stdin.readline
-
-def init(f = None):
-    if os.path.exists("o"): sys.stdout = open("o", "w")
-    if f is not None: setStdin(f)
-    else:
-        if len(sys.argv) == 1:
-            if os.path.isfile("in/i"): setStdin("in/i")
-            elif os.path.isfile("i"): setStdin("i")
-        elif len(sys.argv) == 2: setStdin(sys.argv[1])
-        else: assert False, "Too many sys.argv: %d" % len(sys.argv)
 
 def dprint(*args):
     if DEBUG: print(*args)
@@ -29,11 +18,12 @@ def parr(arr):
     for i in arr:
         print(i)
 
-init(None)
+input = sys.stdin.readline
 
 N, R, Q = map(int, input().split())
 R -= 1
 g = [[] for _ in range(N)]
+
 
 for _ in range(N-1):
     #U, V = map(lambda x: int(x)-1, input().split())
