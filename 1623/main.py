fa4c36238f8e5cd664b21894cc1ae9ead3240f18
dqgthb <dqgthb@users.noreply.github.com>
2021-01-16 04:21:53 +0900

    .

--- 1623/main.py
+++ 1623/main.py
@@ -1,3 +1,46 @@
+
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+def copy2d(mat):
+    return [row[:] for row in mat]
+
+def Mat(h, w, default = None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+# CP template Version 1.005
+import os
+import sys
+sys.setrecursionlimit(10**9)
+
+DEBUG = False
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+def init(f = None):
+    global input
+    input = sys.stdin.readline # by default
+    if os.path.exists("o"): sys.stdout = open("o", "w")
+    if f is not None: setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"): setStdin("in/i")
+            elif os.path.isfile("i"): setStdin("i")
+        elif len(sys.argv) == 2: setStdin(sys.argv[1])
+        else: assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+def pfast(*args, end = "\n", sep=' '): sys.stdout.write(sep.join(map(str, args)) + end)
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
 input = sys.stdin.readline # by default
 
 
@@ -61,46 +104,3 @@ lst = []
 dfs(0, 0)
 lst.sort()
 pfast(*lst, -1)
-
-
-
-
-def For(*args):
-    return itertools.product(*map(range, args))
-
-def copy2d(mat):
-    return [row[:] for row in mat]
-
-def Mat(h, w, default = None):
-    return [[default for _ in range(w)] for _ in range(h)]
-
-# CP template Version 1.005
-import os
-import sys
-sys.setrecursionlimit(10**9)
-
-DEBUG = False
-
-def setStdin(f):
-    global DEBUG, input
-    DEBUG = True
-    sys.stdin = open(f)
-    input = sys.stdin.readline
-
-def init(f = None):
-    global input
-    input = sys.stdin.readline # by default
-    if os.path.exists("o"): sys.stdout = open("o", "w")
-    if f is not None: setStdin(f)
-    else:
-        if len(sys.argv) == 1:
-            if os.path.isfile("in/i"): setStdin("in/i")
-            elif os.path.isfile("i"): setStdin("i")
-        elif len(sys.argv) == 2: setStdin(sys.argv[1])
-        else: assert False, "Too many sys.argv: %d" % len(sys.argv)
-
-def pfast(*args, end = "\n", sep=' '): sys.stdout.write(sep.join(map(str, args)) + end)
-
-def parr(arr):
-    for i in arr:
-        print(i)
