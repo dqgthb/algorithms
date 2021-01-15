507720e77cddaed80591de3db9f56b204021706c
dqgthb <dqgthb@users.noreply.github.com>
2021-01-16 03:40:52 +0900

    .

--- 1623/main.py
+++ 1623/main.py
@@ -1,6 +1,5 @@
 def main(f = None):
     init(f)
-    cache = [[-1]*2 for _ in range(200000)]
 
     def sol(cur, flag):
         val = cache[cur][flag]
@@ -24,6 +23,7 @@ def main(f = None):
     parentOf = [None] + [int(i)-1 for i in input().split()]
     for i in range(1, N):
         tree[parentOf[i]].append(i)
+    cache = [[-1]*2 for _ in range(N)]
     
     print(sol(0, 1), sol(0, 0))
 
