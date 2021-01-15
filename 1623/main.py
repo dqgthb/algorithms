cff08e2d7e70ab9df77eb0e0b5774ae3ea43c645
dqgthb <dqgthb@users.noreply.github.com>
2021-01-16 04:22:52 +0900

    .

--- 1623/main.py
+++ 1623/main.py
@@ -43,64 +43,68 @@ def parr(arr):
 
 input = sys.stdin.readline # by default
 
+def main():
 
-def sol(cur, flag):
-    val = cache[cur][flag]
-    if val != -1: return val
-    val = 0
+    def sol(cur, flag):
+        val = cache[cur][flag]
+        if val != -1: return val
+        val = 0
 
-    if flag:
-        val += cost[cur]
-        for nxt in tree[cur]:
-            val += sol(nxt, 0)
-    
-    else:
-        for nxt in tree[cur]:
-            val += max(sol(nxt, 0), sol(nxt, 1))
-    cache[cur][flag] = val
-    return val
+        if flag:
+            val += cost[cur]
+            for nxt in tree[cur]:
+                val += sol(nxt, 0)
+        
+        else:
+            for nxt in tree[cur]:
+                val += max(sol(nxt, 0), sol(nxt, 1))
+        cache[cur][flag] = val
+        return val
 
-def dfs(cur):
-    for child in tree[cur]:
-        dfs(child)
-    
-    if not tree[cur]:
-        cache[cur][0] = 0
-        cache[cur][1] = cost[cur]
-    else:
-        cache[cur][0] = sum(list(max(cache[x]) for x in tree[cur]))
-        cache[cur][1] = cost[cur] + sum(cache[x][0] for x in tree[cur])
+    def dfs(cur):
+        for child in tree[cur]:
+            dfs(child)
+        
+        if not tree[cur]:
+            cache[cur][0] = 0
+            cache[cur][1] = cost[cur]
+        else:
+            cache[cur][0] = sum(list(max(cache[x]) for x in tree[cur]))
+            cache[cur][1] = cost[cur] + sum(cache[x][0] for x in tree[cur])
 
-N = int(input())
-tree = [[] for _ in range(N)]
-cost = [int(i) for i in input().split()]
-parentOf = [None] + [int(i)-1 for i in input().split()]
-for i in range(1, N):
-    tree[parentOf[i]].append(i)
-del parentOf
-cache = [[-1]*2 for _ in range(N)]
-dfs(0)
-#print(sol(0, 1), sol(0, 0))
-print(cache[0][1], cache[0][0])
-del cost
+    N = int(input())
+    tree = [[] for _ in range(N)]
+    cost = [int(i) for i in input().split()]
+    parentOf = [None] + [int(i)-1 for i in input().split()]
+    for i in range(1, N):
+        tree[parentOf[i]].append(i)
+    del parentOf
+    cache = [[-1]*2 for _ in range(N)]
+    dfs(0)
+    #print(sol(0, 1), sol(0, 0))
+    print(cache[0][1], cache[0][0])
+    del cost
 
-def dfs(cur, flag):
-    if flag:
-        for nxt in tree[cur]:
-            dfs(nxt, 0)
-    else:
-        for nxt in tree[cur]:
-            if cache[nxt][0] > cache[nxt][1]:
+    def dfs(cur, flag):
+        if flag:
+            for nxt in tree[cur]:
                 dfs(nxt, 0)
-            else:
-                dfs(nxt, 1)
-                lst.append(nxt+1)
-lst = [1]
-dfs(0, 1)
-lst.sort()
-pfast(*lst, -1)
+        else:
+            for nxt in tree[cur]:
+                if cache[nxt][0] > cache[nxt][1]:
+                    dfs(nxt, 0)
+                else:
+                    dfs(nxt, 1)
+                    lst.append(nxt+1)
+    lst = [1]
+    dfs(0, 1)
+    lst.sort()
+    pfast(*lst, -1)
 
-lst = []
-dfs(0, 0)
-lst.sort()
-pfast(*lst, -1)
+    lst = []
+    dfs(0, 0)
+    lst.sort()
+    pfast(*lst, -1)
+
+if __name__ == "__main__":
+    main()
