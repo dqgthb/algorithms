385bf5f4c114252d8f1f5ce96aa8b59a777aa8d2
dk <kimdhyungg@gmail.com>
2021-01-19 15:20:53 +0900

    .

--- 13458/explain.py
+++ 13458/explain.py
@@ -8,24 +8,3 @@ while i < len(E):
     E[i] = arr[i] * 2
     i += 1
 print(E)
-
-
-D = [0, 0, 0, 0, 0]
-for i in range(len(D)):
-    print(i, end = ' ')
-    D[i] = arr[i] * 2
-print(D)
-
-
-C = []
-for i in arr:
-    C.append(i*2)
-print(C)
-
-
-A = list(map(lambda x: x*2, arr))
-print(A)
-
-
-B = [i*2 for i in arr]
-print(B)
