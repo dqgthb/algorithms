42e251855d67ae96b9df9732dc68abec6f98dbcb
dqgthb <dqgthb@users.noreply.github.com>
2021-01-19 20:37:53 +0900

    .

--- programmers/learn-courses-30-lessons-64062/main.py
+++ programmers/learn-courses-30-lessons-64062/main.py
@@ -1,24 +1,35 @@
 def solution(stones, k):
     count = 0
-    while countConsecutiveZeros(stones) < k:
-        min_ = min(i for i in stones if i != 0)
-        stones = [i - min_ if i - min_ >= 0 else 0 for i in stones]
-        count += min_
-    return count
+    ans = binRight(k, min(stones), max(stones), stones)
 
-def countConsecutiveZeros(stones):
-    count = 0
+    def binRight(val, left, right, stones):
+    
+    if left == right:
+        left
+        
+    mid = (left + right) // 2
+    midVal = ccz(stones, mid)
+        
+    if val < midVal:
+        return binRight(val, left, mid, stones)
+    else:
+        return binRight(val, mid+1, right, stones)
+        
+    
+    
+def ccz(stones, people):
+    stones = [stone - people for stone in stones]
+    
     maxCount = 0
+    count = 0
     for stone in stones:
-        if stone == 0:
+        if stone <= 0:
             count += 1
-            maxCount = max(count, maxCount)
+            maxCount = max(maxCount, count)
         else:
             count = 0
     return maxCount
-
-
+    
+    
 stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
 k = 3
-ans = solution(stones, k)
-print(ans)
