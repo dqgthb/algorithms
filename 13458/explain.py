318e9ccba9b293e0c5a128dd0e00c7289a5f5f52
dk <kimdhyungg@gmail.com>
2021-01-19 15:19:53 +0900

    .

--- 13458/explain.py
+++ 13458/explain.py
@@ -4,6 +4,7 @@ arr = [1, 2, 3, 4, 5]
 E = [0, 0, 0, 0, 0]
 i = 0
 while i < len(E):
+    print(i, end = ' ')
     E[i] = arr[i] * 2
     i += 1
 print(E)
@@ -11,18 +12,20 @@ print(E)
 
 D = [0, 0, 0, 0, 0]
 for i in range(len(D)):
+    print(i, end = ' ')
     D[i] = arr[i] * 2
 print(D)
+
 
 C = []
 for i in arr:
     C.append(i*2)
 print(C)
 
-def multiplyBy2(x):
-    return x*2
-A = list(map(multiplyBy2, arr))
+
+A = list(map(lambda x: x*2, arr))
 print(A)
+
 
 B = [i*2 for i in arr]
 print(B)
