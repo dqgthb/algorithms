e0faeeca84048339215c9d7c6f0fc2f7b5e6471c
dk <kimdhyungg@gmail.com>
2021-01-19 15:17:53 +0900

    .

--- 13458/explain.py
+++ 13458/explain.py
@@ -1,15 +1,19 @@
 arr = [1, 2, 3, 4, 5]
 
 
+
+
+D = [0, 0, 0, 0, 0]
+for i in range(len(D)):
+    D[i] = arr[i] * 2
+
 C = []
 for i in arr:
     C.append(i*2)
 print(C)
 
-
 def multiplyBy2(x):
     return x*2
-
 A = list(map(multiplyBy2, arr))
 print(A)
 
