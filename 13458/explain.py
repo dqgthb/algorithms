4f1bacee7654c71653f4864d9d6e36ed20184aa1
dk <kimdhyungg@gmail.com>
2021-01-19 15:18:53 +0900

    .

--- 13458/explain.py
+++ 13458/explain.py
@@ -1,11 +1,18 @@
 arr = [1, 2, 3, 4, 5]
 
 
+E = [0, 0, 0, 0, 0]
+i = 0
+while i < len(E):
+    E[i] = arr[i] * 2
+    i += 1
+print(E)
 
 
 D = [0, 0, 0, 0, 0]
 for i in range(len(D)):
     D[i] = arr[i] * 2
+print(D)
 
 C = []
 for i in arr:
