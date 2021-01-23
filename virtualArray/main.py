bd49cb4a8a71a377a84943132ee10c38a23ff537
dqgthb <dqgthb@users.noreply.github.com>
2021-01-23 20:42:52 +0900

    .

--- virtualArray/main.py
+++ virtualArray/main.py
@@ -1,9 +1,6 @@
-arr = [0, 2, 4, 6, 8]
+class MultiplyByTwo:
+    def __getitem__(self, n):
+        return n*2
 
-arr[0] == 0
-arr[1] == 2
-arr[2] == 4
-arr[3] == 6
-arr[4] == 8
-arr[5] == ?? # index out of bound error
-arr[1000000000] == ??
+arr = MultiplyByTwo()
+print(arr[10000000000])
