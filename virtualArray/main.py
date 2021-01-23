6d5d6ceee6db052f2d32ef3b61058a16ca158ce3
dqgthb <dqgthb@users.noreply.github.com>
2021-01-23 20:39:52 +0900

    .

--- virtualArray/main.py
+++ virtualArray/main.py
@@ -2,9 +2,8 @@ arr = [0, 2, 4, 6, 8]
 
 arr[0] == 0
 arr[1] == 2
-arr[4] == 4
-arr[6] == 6
-arr[8] == 8
+arr[2] == 4
+arr[3] == 6
+arr[4] == 8
 arr[5] == ?? # index out of bound error
-
-lk
+arr[1000000000] == ??
