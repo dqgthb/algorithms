92f6ffcbfdf38cae77c065da9fd7d564f3e0f4ed
dk <kimdhyungg@gmail.com>
2021-01-27 14:16:53 +0900

    .

--- 14501/main.py
+++ 14501/main.py
@@ -30,8 +30,6 @@ def main(f=None):
         T, P = map(int, input().split())
         t.append(T)
         p.append(P if n + T <= N + 1 else 0)
-    print(t)
-    print(p)
     dp = [0] * (N+1)
 
     # ######## INPUT AREA END ############
