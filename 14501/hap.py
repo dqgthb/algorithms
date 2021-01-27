306558b537cd625ecd7a5f40fb10d27684c4274e
dqgthb <dqgthb@users.noreply.github.com>
2021-01-27 18:29:52 +0900

    .

--- 14501/hap.py
+++ 14501/hap.py
@@ -29,11 +29,12 @@ def solution(t, p):
             #dp[j] = max(dp[j], dp[i] + consult[j][1])
             dp[j] = max(dp[j], dp[i] + p[i])
     # dp 생성 완료 후, T(상담이 걸리는 기간) 을 따져서, 조건을 만족시키는 가장 큰 수를 반환하고자 함
-    print(dp)
+    max_ = 0
     for i in range(n-1,-1,-1):
         #if consult[i][0] <= n-i: # 현재 인덱스에 해당하는 T 를 더했을때 배열 범위를 벗어나지 않는다면
         if t[i] <= n-i: # 현재 인덱스에 해당하는 T 를 더했을때 배열 범위를 벗어나지 않는다면
-            return dp[i]
+            max_ = max(dp[i], max_)
+    return max_
 
 
 n = int(input())
@@ -44,7 +45,5 @@ for i in range(n):
     T, P = map(int, input().split())
     t.append(T)
     p.append(P)
-print(t)
-print(p)
 #print(solution(consult))
 print(solution(t, p))
