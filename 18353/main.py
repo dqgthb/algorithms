01f9e8a2899a318efe1af4a7611daa0b2317e569
dk <kimdhyungg@gmail.com>
2021-01-27 15:21:52 +0900

    .

--- 18353/main.py
+++ 18353/main.py
@@ -34,13 +34,12 @@ def main(f=None):
     DPsolution()
 
 def DPsolution():
-    dp = [-1] * N
-    dp[0] = 1
+    dp = [1] * N
     for i in range(1, N):
         for j in range(i):
             if arr[i] > arr[j]:
                 dp[i] = max(dp[i], dp[j]+1)
-    print(dp)
+    print(N - max(dp))
 
 
 
