0b0d557285eda03a9ccee3a2faffe3e76a1cabd1
dk <kimdhyungg@gmail.com>
2021-01-27 15:20:52 +0900

    .

--- 18353/main.py
+++ 18353/main.py
@@ -30,12 +30,17 @@ def main(f=None):
     # ######## INPUT AREA END ############
     # ####################################
 
+    # binarySearchSolution()
+    DPsolution()
+
 def DPsolution():
     dp = [-1] * N
     dp[0] = 1
     for i in range(1, N):
         for j in range(i):
-            dp[i] =
+            if arr[i] > arr[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+    print(dp)
 
 
 
