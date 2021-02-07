71c11f858980b8f0cf77ef897f12ab63ddf84bfb
dqgthb <dqgthb@users.noreply.github.com>
2021-02-08 03:48:08 +0900

    .

---
+++ programmers/learn-courses-30-lessons-72413-합승택시요금/main.py
@@ -0,0 +1,217 @@
+# CP template Version 1.006
+import os
+import sys
+import itertools
+import collections
+import string
+# not for python < 3.9
+# from functools import cmp_to_key, reduce, partial, cache
+from functools import cmp_to_key, reduce, partial
+from itertools import product
+from collections import deque, Counter, defaultdict as dd
+from math import log, log2, ceil, floor, gcd, sqrt
+import math
+from heapq import heappush, heappop
+from bisect import bisect_left as bl, bisect_right as br
+DEBUG = False
+
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ####################################
+    # ######## INPUT AREA BEGIN ##########
+
+    # ######## INPUT AREA END ############
+    # ####################################
+    test1()
+
+def test1():
+    n = 6
+    s = 4
+    a = 6
+    b = 2
+    fares = [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]
+    ans = solution(n, s, a, b, fares)
+    assert ans == 82, "test1 not correct"
+
+
+def test2():
+    n = 7
+    s, a, b = 3, 4, 1
+    fares = [[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]
+    ans = solution(n, s, a, b, fares)
+    assert ans == 14, "test2 not correct"
+
+
+def solution(n, s, a, b, fares):
+    s -= 1
+    a -= 1
+    b -= 1
+
+    mat = [[10**9 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        mat[i][i] = 0
+
+    for from_, to, fare in fares:
+        from_ -= 1
+        to -= 1
+        mat[from_][to] = fare
+        mat[to][from_] = fare
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                val = mat[i][k] + mat[k][j]
+                if mat[i][j] > val:
+                    mat[i][j] = val
+
+    sum_ = 10 ** 9
+    for i in range(n):
+        val = mat[s][i] + mat[i][a] +mat[i][b]
+        if sum_ > val:
+            sum_ = val
+
+    return sum_
+
+# #############################################################################
+# #############################################################################
+# ############################## TEMPLATE AREA ################################
+# #############################################################################
+# #############################################################################
+
+enu = enumerate
+
+
+def argmax(arr):
+    return max(enumerate(arr), key=lambda x: x[1])
+
+
+def argmin(arr):
+    return min(enumerate(arr), key=lambda x: x[1])
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def copy2d(mat):
+    return [row[:] for row in mat]
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+# Mod #
+class Mod:
+    MOD = 10**9 + 7
+    maxN = 5
+    FACT = [0] * maxN
+    INV_FACT = [0] * maxN
+
+    @staticmethod
+    def setMOD(n): Mod.MOD = n
+
+    @staticmethod
+    def add(x, y): return (x+y) % Mod.MOD
+
+    @staticmethod
+    def multiply(x, y): return (x*y) % Mod.MOD
+
+    @staticmethod
+    def power(x, y):
+        if y == 0:
+            return 1
+        elif y % 2:
+            return Mod.multiply(x, Mod.power(x, y-1))
+        else:
+            a = Mod.power(x, y//2)
+            return Mod.multiply(a, a)
+
+    @staticmethod
+    def inverse(x):
+        return Mod.power(x, Mod.MOD-2)
+
+    @staticmethod
+    def divide(x, y):
+        return Mod.multiply(x, Mod.inverse(y))
+
+    @staticmethod
+    def allFactorials():
+        Mod.FACT[0] = 1
+        for i in range(1, Mod.maxN):
+            Mod.FACT[i] = Mod.multiply(i, Mod.FACT[i-1])
+
+    @staticmethod
+    def inverseFactorials():
+        n = len(Mod.INV_FACT)
+        Mod.INV_FACT[n-1] = Mod.inverse(Mod.FACT[n-1])
+        for i in range(n-2, -1, -1):
+            Mod.INV_FACT[i] = Mod.multiply(Mod.INV_FACT[i+1], i+1)
+
+    @staticmethod
+    def coeffBinom(n, k):
+        if n < k:
+            return 0
+        return Mod.multiply(Mod.FACT[n],
+                            Mod.multiply(Mod.INV_FACT[k], Mod.INV_FACT[n-k]))
+
+    @staticmethod
+    def sum(it):
+        res = 0
+        for i in it:
+            res = Mod.add(res, i)
+        return res
+# END Mod #
+
+
+def dprint(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
