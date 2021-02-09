bd3025d000f81baa7bea5b04fa0bea2f771e65c0
dqgthb <dqgthb@users.noreply.github.com>
2021-02-09 23:15:00 +0900

    .

---
+++ 7569/main.py
@@ -0,0 +1,215 @@
+# CP template Version 1.006
+import os
+import sys
+import itertools
+import collections
+import string
+# not for python < 3.9
+# from functools import cmp_to_key, reduce, partial, cache
+from functools import cmp_to_key, reduce, partial
+from itertools import product
+from collections import deque, Counter, defaultdict as dd
+from math import log, log2, ceil, floor, gcd, sqrt
+import math
+from heapq import heappush, heappop
+from bisect import bisect_left as bl, bisect_right as br
+DEBUG = False
+
+propagate = (
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, -1, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+)
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ####################################
+    # ######## INPUT AREA BEGIN ##########
+
+    global M, N, H
+    M, N, H = map(int, input().split())
+    box = [None for _ in range(H)]
+    for i in range(H):
+        box[i] = [list(map(int, input().split())) for _ in range(N)]
+
+    dq = deque()
+    for h in range(H):
+        for n in range(N):
+            for m in range(M):
+                if box[h][n][m] == 1:
+                    dq.append((h, n, m, 0))
+
+    maxTime = 0
+    while dq:
+        h, n, m, time = dq.popleft()
+        maxTime = max(time, maxTime)
+
+        for dh, dm, dn in propagate:
+            nh = h + dh
+            nn = n + dn
+            nm = m + dm
+            if 0 <= nh < H and 0 <= nm < M and 0 <= nn < N:
+                if box[nh][nn][nm] == 0:
+                    box[nh][nn][nm] = 1
+                    dq.append((nh, nn, nm, time + 1))
+
+    for h, n, m in For(H, N, M):
+        if box[h][n][m] == 0:
+            print("-1")
+            return
+    print(maxTime)
+
+
+
+
+
+
+    # ######## INPUT AREA END ############
+    # ####################################
+
+
+# #############################################################################
+# #############################################################################
+# ############################## TEMPLATE AREA ################################
+# #############################################################################
+# #############################################################################
+
+enu = enumerate
+
+
+def argmax(arr):
+    return max(enumerate(arr), key=lambda x: x[1])
+
+
+def argmin(arr):
+    return min(enumerate(arr), key=lambda x: x[1])
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def copy2d(mat):
+    return [row[:] for row in mat]
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+# Mod #
+class Mod:
+    MOD = 10**9 + 7
+    maxN = 5
+    FACT = [0] * maxN
+    INV_FACT = [0] * maxN
+
+    @staticmethod
+    def setMOD(n): Mod.MOD = n
+
+    @staticmethod
+    def add(x, y): return (x+y) % Mod.MOD
+
+    @staticmethod
+    def multiply(x, y): return (x*y) % Mod.MOD
+
+    @staticmethod
+    def power(x, y):
+        if y == 0:
+            return 1
+        elif y % 2:
+            return Mod.multiply(x, Mod.power(x, y-1))
+        else:
+            a = Mod.power(x, y//2)
+            return Mod.multiply(a, a)
+
+    @staticmethod
+    def inverse(x):
+        return Mod.power(x, Mod.MOD-2)
+
+    @staticmethod
+    def divide(x, y):
+        return Mod.multiply(x, Mod.inverse(y))
+
+    @staticmethod
+    def allFactorials():
+        Mod.FACT[0] = 1
+        for i in range(1, Mod.maxN):
+            Mod.FACT[i] = Mod.multiply(i, Mod.FACT[i-1])
+
+    @staticmethod
+    def inverseFactorials():
+        n = len(Mod.INV_FACT)
+        Mod.INV_FACT[n-1] = Mod.inverse(Mod.FACT[n-1])
+        for i in range(n-2, -1, -1):
+            Mod.INV_FACT[i] = Mod.multiply(Mod.INV_FACT[i+1], i+1)
+
+    @staticmethod
+    def coeffBinom(n, k):
+        if n < k:
+            return 0
+        return Mod.multiply(Mod.FACT[n],
+                            Mod.multiply(Mod.INV_FACT[k], Mod.INV_FACT[n-k]))
+
+    @staticmethod
+    def sum(it):
+        res = 0
+        for i in it:
+            res = Mod.add(res, i)
+        return res
+# END Mod #
+
+
+def dprint(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
