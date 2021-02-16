122b734d2f529fec2db1359a58505783d859ce0a
dk <kimdhyungg@gmail.com>
2021-02-16 15:04:53 +0900

    .

--- 1043/main.py
+++ 1043/main.py
@@ -26,7 +26,7 @@ def main(f=None):
     arr = [int(i) for i in input().split()]
     knowsTruth = set()
     numTruth = arr[0]
-    for i in range(1, numTruth+1):
+    for i in range(1, len(arr)):
         knowsTruth.add(arr[i])
 
     # ######## INPUT AREA END ############
