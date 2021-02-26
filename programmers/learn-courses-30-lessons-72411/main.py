14ed46122fc58c4edd0636e3cf16fdc035f057cf
dk <kimdhyungg@gmail.com>
2021-02-26 10:49:52 +0900

    .

--- programmers/learn-courses-30-lessons-72411/main.py
+++ programmers/learn-courses-30-lessons-72411/main.py
@@ -2,31 +2,6 @@
 import itertools
 
 def solution(orders, course):
-    answer = []
-    menus = set(menu for order in orders for menu in order)
-    menus = list(menus)
-    menus.sort()
-    #orderSet = [set(order) for order in orders]
-    for num in course:
-        maxCount = 2
-        maxCandidates = []
-        for course in itertools.combinations(menus, r=num):
-            count = 0
-            for order in orders:
-                for menu in course:
-                    if menu not in order:
-                        break
-                else:
-                    count += 1
-            if count == maxCount:
-                maxCandidates.append(course)
-            elif count > maxCount:
-                maxCandidates = [course]
-                maxCount = count
-        for candidate in maxCandidates:
-            answer.append(''.join(candidate))
-    answer.sort()
-    return answer
 
 
 def main():
