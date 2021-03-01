f071cb4f34d240bd8d258e378fa4a741ea90e564
dk <kimdhyungg@gmail.com>
2021-03-01 14:41:53 +0900

    .

--- 10942/main.py
+++ 10942/main.py
@@ -32,7 +32,7 @@ def main(f=None):
 
     for i in range(N):
         for j in range(i, N):
-            dp(i, j)
+            #dp(i, j)
 
     for _ in range(M):
         S, E = map(lambda x: int(x) - 1, input().split())
