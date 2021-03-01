129b22ba8d053f3ab78b9159f83dd30652b7cbb7
dk <kimdhyungg@gmail.com>
2021-03-01 14:47:53 +0900

    .

--- 10942/main.py
+++ 10942/main.py
@@ -30,8 +30,12 @@ def main(f=None):
     for i in range(N):
         DP[i][i] = True
 
-    for i i range(n-1):
+    for i in range(N-1):
+        DP[i][i+1] = arr[i] == arr[i+1]
+    parr(DP)
 
+    for i in range(2, N):
+        for j in range(i, N):
 
     for _ in range(M):
         S, E = map(lambda x: int(x) - 1, input().split())
