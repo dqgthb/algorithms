12891595877de6dfaf4ba98a3669e0cd70960451
dqgthb <dqgthb@users.noreply.github.com>
2021-03-23 19:17:52 +0900

    .

--- 19591/main.py
+++ 19591/main.py
@@ -30,7 +30,7 @@ def main(f=None):
 
     print(equation)
     #parse = equation.split("*+-/")
-    parse = re.split("+-*/", equation)
+    parse = re.split(r"+|-|*", equation)
     print(parse)
 
 
