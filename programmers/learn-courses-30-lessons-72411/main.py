2422974e99024e65d2ba59b128b60e23c753bd85
dk <kimdhyungg@gmail.com>
2021-05-03 13:45:52 +0900

    .

--- programmers/learn-courses-30-lessons-72411/main.py
+++ programmers/learn-courses-30-lessons-72411/main.py
@@ -1,23 +1,41 @@
-
+import collections
 import itertools
 
 def solution(orders, course):
-    menus = set(menu for order in orders for menu in order)
-    print(menus)
+    orders = [list(order) for order in orders]
+    for order in orders:
+        order.sort()
 
-    for menu in course:
+    orderSet = [set(order) for order in orders]
 
+    courses = []
 
-
-def courseMenu(numOfCourse):
-
-
-
-
+    for courseNum in course:
+        alreadyConsidered = set()
+        counter = collections.Counter()
+        for orderNum, order in enumerate(orders):
+            for courseCandidate in itertools.combinations(order, r = courseNum):
+                courseCandidate = tuple(sorted(courseCandidate))
+                if courseCandidate in alreadyConsidered:
+                    continue
+                else:
+                    alreadyConsidered.add(courseCandidate)
+                    for nextOrderNum in range(orderNum + 1, len(orderSet)):
+                        order = orderSet[nextOrderNum]
+                        if set(courseCandidate) <= order:
+                            counter[courseCandidate] += 1
+        if len(counter) > 0:
+            maxFreq = max(counter.values())
+            for course in counter:
+                if counter[course] == maxFreq:
+                    courses.append(course)
+    courses = [''.join(course) for course in courses]
+    courses.sort()
+    return courses
 
 
 def main():
-    ans = solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4])
+    #ans = solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4])
     ans = solution(["XYZ", "XWY", "WXA"], [2, 3, 4])
     print(ans)
 
