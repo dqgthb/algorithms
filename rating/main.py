e6d8f82391551890b8dcd8521ff8402c43eea71e
dqgthb <dqgthb@users.noreply.github.com>
2021-08-21 17:37:52 +0900

    .

--- rating/main.py
+++ rating/main.py
@@ -2,6 +2,8 @@
 bonus = 0
 
 for i in range(1300):
-        val = round(175 * (1 - 0.995 ** i))
-    print(i, val)
+    val = round(175 * (1 - 0.995 ** i))
+    if bonus != val:
+        print(i, val)
+        bonus = val
 
