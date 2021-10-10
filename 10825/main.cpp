6cfebdd5834d7a3ef0fcfaa1f971e81d2c922e32
dqgthb <dqgthb@users.noreply.github.com>
2021-10-11 02:34:54 +0900

    .

--- 10825/main.cpp
+++ 10825/main.cpp
@@ -1,5 +1,127 @@
+#ifndef _GLIBCXX_NO_ASSERT
+    #include <cassert>
+    #endif
+    #include <cctype>
+    #include <cerrno>
+    #include <cfloat>
+    #include <ciso646>
+    #include <climits>
+    #include <clocale>
+    #include <cmath>
+    #include <csetjmp>
+    #include <csignal>
+    #include <cstdarg>
+    #include <cstddef>
+    #include <cstdio>
+    #include <cstdlib>
+    #include <cstring>
+    #include <ctime>
 
-int main(int argc, char const *argv[]) {
-    /* code */
+    #if __cplusplus >= 201103L
+    #include <ccomplex>
+    #include <cfenv>
+    #include <cinttypes>
+    #include <cstdbool>
+    #include <cstdint>
+    #include <ctgmath>
+    #include <cwchar>
+    #include <cwctype>
+    #endif
+
+    // C++
+    #include <algorithm>
+    #include <bitset>
+    #include <complex>
+    #include <deque>
+    #include <exception>
+    #include <fstream>
+    #include <functional>
+    #include <iomanip>
+    #include <ios>
+    #include <iosfwd>
+    #include <iostream>
+    #include <istream>
+    #include <iterator>
+    #include <limits>
+    #include <list>
+    #include <locale>
+    #include <map>
+    #include <memory>
+    #include <new>
+    #include <numeric>
+    #include <ostream>
+    #include <queue>
+    #include <set>
+    #include <sstream>
+    #include <stack>
+    #include <stdexcept>
+    #include <streambuf>
+    #include <string>
+    #include <typeinfo>
+    #include <utility>
+    #include <valarray>
+    #include <vector>
+
+    #if __cplusplus >= 201103L
+    #include <array>
+    #include <atomic>
+    #include <chrono>
+    #include <condition_variable>
+    #include <forward_list>
+    #include <future>
+    #include <initializer_list>
+    #include <mutex>
+    #include <random>
+    #include <ratio>
+    #include <regex>
+    #include <scoped_allocator>
+    #include <system_error>
+    #include <thread>
+    #include <tuple>
+    #include <typeindex>
+    #include <type_traits>
+    #include <unordered_map>
+    #include <unordered_set>
+#endif
+#define MOD 1000000007
+#define test int t; cin>>t; while(t--)
+#define init(arr,val) memset(arr,val,sizeof(arr))
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopr(i,a,b) for(int i=a;i>=b;i--)
+#define loops(i,a,b,step) for(int i=a;i<b;i+=step)
+#define looprs(i,a,b,step) for(int i=a;i>=b;i-=step)
+#define ull unsigned long long int
+#define ll long long int
+#define P pair
+#define PLL pair<long long, long long>
+#define PII pair<int, int>
+#define PUU pair<unsigned long long int, unsigned long long int>
+#define L list
+#define V vector
+#define D deque
+#define ST set
+#define MS multiset
+#define M map
+#define UM unordered_map
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define MM multimap
+#define F first
+#define S second
+#define IT iterator
+#define RIT reverse_iterator
+#define FAST ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define FILE_READ_IN freopen("input.txt","r",stdin);
+#define FILE_READ_OUT freopen("output.txt","w",stdout);
+#define all(a) a.begin(),a.end()
+#define ld long double
+using namespace std;
+const ll maxn = 1e5;
+const ll inf = 1e9;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello world" << endl;
     return 0;
 }
