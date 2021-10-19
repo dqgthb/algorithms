f7df9a0448178f3db0336de9e1acda27320befcf
dqgthb <dqgthb@users.noreply.github.com>
2021-10-20 02:15:51 +0900

    .

--- 2632/main.py
+++ 2632/main.py
@@ -52,6 +52,8 @@ def main(f=None):
     for i in pA:
         l = bl(pB, C-i)
         r = br(pB, C-i)
+        if r - l > 0:
+            print(i, C-i, l, r)
         cnt += r - l
     print(cnt)
 
