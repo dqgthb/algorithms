30048bc2dab7a0834704b6f91d4df629214d5264
dqgthb <dqgthb@users.noreply.github.com>
2021-10-22 01:54:43 +0900

    .

---
+++ programmers/81305/main.py
@@ -0,0 +1,131 @@
+# CP template Version 1.006
+import os
+import sys
+#import string
+#from functools import cmp_to_key, reduce, partial
+#import itertools
+#from itertools import product
+#import collections
+#from collections import deque
+#from collections import Counter, defaultdict as dd
+#import math
+#from math import log, log2, ceil, floor, gcd, sqrt
+#from heapq import heappush, heappop
+#import bisect
+#from bisect import bisect_left as bl, bisect_right as br
+DEBUG = False
+
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ######## INPUT AREA BEGIN ##########
+
+    k = 3
+    num = [12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1]
+    links = [[-1, -1], [-1, -1], [-1, -1], [-1, -1], [8, 5], [2, 10], [3, 0], [6, 1], [11, -1], [7, 4], [-1, -1], [-1, -1]]
+
+    ans = solution(k, num, links)
+    print(ans)
+
+
+    # ######## INPUT AREA END ############
+
+
+def solution(k, num, links):
+    global cum
+    vis = [False] * len(num)
+    for i, j in links:
+        if i != -1:
+            vis[i] = True
+        if j != -1:
+            vis[j] = True
+
+    root = -1
+    for i, e in enu(vis):
+        if not e:
+            root = i
+
+    solve(root, k, num, links)
+
+
+def solve(node, k, num, links):
+    if node == -1:
+        return 0
+    left, right = links[node]
+    if k == 0:
+        return num[node] + solve(left, 0, num, links) + solve(right, 0, num, links)
+
+
+
+
+
+
+
+
+
+
+
+# TEMPLATE ###############################
+
+
+enu = enumerate
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+def pr(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
