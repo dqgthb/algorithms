48b3dd21304e831c92f72b20e6fe2911ad010b23
dqgthb <dqgthb@users.noreply.github.com>
2021-11-13 15:17:54 +0900

    .

--- jang/dijkstra/main.py
+++ jang/dijkstra/main.py
@@ -1,6 +1,13 @@
 
 
 def main():
+
+    airports = []
+    with open("Hawaiian-airports.txt") as f:
+        for line
+    with open("Hawaiian-flights.txt") as f:
+        for line in f:
+            print(line.strip())
     G = []
 
 
