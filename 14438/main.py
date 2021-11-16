5bd10d893c7e3104915f142b4a1af4453f2df17e
dqgthb <dqgthb@users.noreply.github.com>
2021-11-17 02:08:54 +0900

    .

--- 14438/main.py
+++ 14438/main.py
@@ -21,86 +21,9 @@ def main(f=None):
     # sys.setrecursionlimit(10**9)
     # ######## INPUT AREA BEGIN ##########
 
-    global N, K, A, T
-    while True:
-        try:
-            N, K = map(int, input().split())
-        except:
-            return
-        A = [int(i) for i in input().split()]
-        for i in range(N):
-            a = A[i]
-            if a > 0:
-                A[i] = 1
-            elif a < 0:
-                A[i] = -1
 
-        T = [None] * (4 * N)
-        segInit(0, N-1, 1)
-
-        ans = []
-
-        for _ in range(K):
-            a, b, c = input().split()
-            b = int(b)
-            c = int(c)
-
-            if a == 'C':
-                b -= 1
-                if c > 0:
-                    c = 1
-                elif c < 0:
-                    c = -1
-                change(0, N-1, 1, b, c)
-            else:
-                b -= 1
-                c -= 1
-                res = product(0, N-1, 1, b, c)
-                if res > 0:
-                    ans.append("+")
-                elif res == 0:
-                    ans.append("0")
-                else:
-                    ans.append("-")
-        print("".join(ans))
 
     # ######## INPUT AREA END ############
-
-
-def segInit(s, e, i):
-    if s == e:
-        T[i] = A[s]
-        return T[i]
-
-    m = (s + e) // 2
-    T[i] = segInit(s, m, i*2) * segInit(m+1, e, i*2+1)
-    return T[i]
-
-
-def change(s, e, i, idx, val):
-
-    if idx < s or e < idx:
-        return T[i]
-
-    if s == e:
-        T[i] = val
-        return T[i]
-
-    m = (s + e) // 2
-    T[i] = change(s, m, i*2, idx, val) * change(m+1, e, i*2+1, idx, val)
-    return T[i]
-
-
-def product(s, e, i, l, r):
-
-    if r < s or e < l:
-        return 1
-
-    if l <= s and e <= r:
-        return T[i]
-
-    m = (s + e) // 2
-    return product(s, m, i*2, l, r) * product(m+1, e, i*2+1, l, r)
 
 
 # TEMPLATE ###############################
