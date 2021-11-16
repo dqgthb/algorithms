3ef4fc9d2b42bb095a320ef4e1644f6e7e33d78f
dqgthb <dqgthb@users.noreply.github.com>
2021-11-17 01:06:52 +0900

    .

--- 5676/main.py
+++ 5676/main.py
@@ -24,6 +24,11 @@ def main(f=None):
     N, K = map(int, input().split())
     A = [int(i) for i in input().split()]
 
+    for _ in range(K):
+
+
+
+
 
 
     # ######## INPUT AREA END ############
