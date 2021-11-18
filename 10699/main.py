e9ceb9702ed38cceabf11a4ad150300e00af17b9
dqgthb <dqgthb@users.noreply.github.com>
2021-11-18 22:30:51 +0900

    .

--- 10699/main.py
+++ 10699/main.py
@@ -24,6 +24,9 @@ def main(f=None):
     from datetime import date
     today = date.today()
     print(today)
+    x = 5
+    print(today.strftime(""))
+    map()
 
     # ######## INPUT AREA END ############
 
