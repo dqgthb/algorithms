6ed8fb351fe8f400dc7f9e65588d70bb4a93a526
dqgthb <dqgthb@users.noreply.github.com>
2021-11-20 22:06:52 +0900

    .

--- 17136/main.py
+++ 17136/main.py
@@ -56,6 +56,8 @@ def update(i, j, k, v):
 
 
 def solve(i, j, cnt) -> None:
+    parr(A)
+    print()
     global min_
 
     allZero = True
@@ -76,8 +78,6 @@ def solve(i, j, cnt) -> None:
     else:
         for k in range(5, 0, -1):
             if papers[k-1] > 0:
-                if i == 4 and j == 4 and k == 2:
-                    parr(A)
                 if canPaste(i, j, k):
                     papers[k-1] -= 1
                     update(i, j, k, 0)
