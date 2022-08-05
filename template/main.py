f94b974c9b16ea711407a5ce4279f8b3c345c64c
Daehyun Kim <kimdhyungg@gmail.com>
2022-08-05 17:22:37 +0900

    better template

--- template/main.py
+++ template/main.py
@@ -1,90 +1,47 @@
-# CP template Version 1.006
-import os
-import sys
-#import string
-#from functools import cmp_to_key, reduce, partial
-#import itertools
-#from itertools import product
-#import collections
+# Python Competitive Programming Template (updated 2022-08-05)
+# Created by dqgthb
+
 #from collections import deque
-#from collections import Counter, defaultdict as dd
-#import math
-#from math import log, log2, ceil, floor, gcd, sqrt
 #from heapq import heappush, heappop
-#import bisect
+
+#from functools import cmp_to_key, reduce, partial
+#from collections import Counter, defaultdict as dd
+#from math import log, log2, ceil, floor, gcd, sqrt
 #from bisect import bisect_left as bl, bisect_right as br
-DEBUG = False
 
 
-def main(f=None):
-    init(f)
+def main():
+    init()
     # sys.setrecursionlimit(10**9)
     # ######## INPUT AREA BEGIN ##########
-
-
-
     # ######## INPUT AREA END ############
 
 
 # TEMPLATE ###############################
 
 
-enu = enumerate
-
-
-def For(*args):
-    return itertools.product(*map(range, args))
+def parr(arr):
+    print('\n'.join(str(i) for i in arr))
 
 
 def Mat(h, w, default=None):
     return [[default for _ in range(w)] for _ in range(h)]
 
 
-def nDim(*args, default=None):
-    if len(args) == 1:
-        return [default for _ in range(args[0])]
-    else:
-        return [nDim(*args[1:], default=default) for _ in range(args[0])]
-
-
-def setStdin(f):
-    global DEBUG, input
-    DEBUG = True
-    sys.stdin = open(f)
-    input = sys.stdin.readline
-
-
-def init(f=None):
+def init():
     global input
-    input = sys.stdin.readline  # by default
-    if os.path.exists("o"):
-        sys.stdout = open("o", "w")
-    if f is not None:
-        setStdin(f)
-    else:
-        if len(sys.argv) == 1:
-            if os.path.isfile("in/i"):
-                setStdin("in/i")
-            elif os.path.isfile("i"):
-                setStdin("i")
-        elif len(sys.argv) == 2:
-            setStdin(sys.argv[1])
-        else:
-            assert False, "Too many sys.argv: %d" % len(sys.argv)
+    from sys import stdin, argv
+    input = stdin.readline  # by default
 
+    if len(argv) == 1:
+        from os import path
+        if path.isfile("i"):
+            stdin = open("i")
+            input = stdin.readline
 
-def pr(*args):
-    if DEBUG:
-        print(*args)
-
-
-def pfast(*args, end="\n", sep=' '):
-    sys.stdout.write(sep.join(map(str, args)) + end)
-
-
-def parr(arr):
-    for i in arr:
-        print(i)
+    elif len(argv) == 2:
+        stdin = open(argv[1])
+        input = stdin.readline
 
 
 if __name__ == "__main__":
